6003580802c63a24fcc2604ceeb5a80410d83cd1
Dat <datpv0997@gmail.com>
2020-05-20 00:37:35 +0700

    tmp selenium

--- document_crawler/spiders_entry.py
+++
@@ -1,7 +0,0 @@
-# !/usr/bin/env python
-from scrapy.cmdline import execute
-
-# execute(['scrapy', 'crawl', 'dantri'])
-execute(['scrapy', 'crawl', 'vtv24'])
-
-
---
+++ scrapy/document_crawler/Test.py
@@ -0,0 +1,6 @@
+
+a = 'https://vtv.vn/timeline-thread/242/trang-1.htm'
+b = 'https://vtv.vn/timeline-thread/242/trang-'
+c = '.htm'
+print(len(b))
+print(a[len(b):-4])
