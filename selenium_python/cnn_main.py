fedadfec01fda51a273b1074b439eaf83acb4bb6
Dat <datpv0997@gmail.com>
2020-05-21 02:43:06 +0700

    tmp merge

--- selenium_python/cnn_main.py
+++ selenium_python/cnn_main.py
@@ -4,16 +4,13 @@ from selenium.webdriver.chrome.options import Options
 options = Options()
 options.add_argument('--headless')
 
-url = 'https://e.vnexpress.net/search?q=covid-19&csrf=2577e02d43ef0d05b4d854dd7243251e'
+url_template = 'https://edition.cnn.com/world/live-news/coronavirus-pandemic-{}-intl/index.html'
+date = '05-17-20'
 
 driver = webdriver.Chrome(executable_path=r"driver/chromedriver", chrome_options=options)
 driver.implicitly_wait(10)
 
-driver.get(url)
-
-WebElement element = driver.findElement(By.id("gbqfd"));
-JavascriptExecutor executor = (JavascriptExecutor)driver;
-executor.executeScript("arguments[0].click();", element);
+driver.get(url_template.format(date))
 
 # elems = driver.find_element_by_xpath("//header[@class=\"post-headlinestyles__Header-sc-2ts3cz-0 kpNfDn\"]//text()")
 elems = driver.find_element_by_tag_name("body")
